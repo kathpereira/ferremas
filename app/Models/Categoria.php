53a55b0949a37bea36edb2300150e31933d2a994
Nikoo2110 <fr.mancillaa@duocuc.cl>
2024-05-31 15:04:44 -0400

    Bodeguero

---
+++ app/Models/Categoria.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table = 'categoria';
+    protected $primaryKey = 'id_categoria';
+    public $timestamps = false;
+
+    protected $fillable = ['nombre_cat'];
+
+    public function productos()
+    {
+        return $this->hasMany(Producto::class, 'id_categoria');
+    }
+}
