fc7100bbccf63a5976da61f2d644062fe81796df
kathpereira <kat.pereira@duocuc.cl>
2024-06-06 03:00:52 -0400

    cards listas

--- app/Http/Controllers/CatalogoController.php
+++ app/Http/Controllers/CatalogoController.php
@@ -13,4 +13,27 @@ class CatalogoController extends Controller
         $productos = Producto::all();
         return view('catalogo', compact('productos'));
     }
+
+    public function store(Request $request)
+    {
+        $producto = new Producto();
+
+        // Validar y manejar la subida de la imagen
+        if ($request->hasFile('imagen')) {
+            $file = $request->file('imagen');
+            $path = $file->store('img', 'public'); // Guarda en storage/app/public/img
+            $producto->imagen = 'storage/' . $path;
+        }
+
+        $producto->nombre_prod = $request->input('nombre_prod');
+        $producto->descripcion = $request->input('descripcion');
+        $producto->precio = $request->input('precio');
+        $producto->stock = $request->input('stock');
+        $producto->marca = $request->input('marca');
+        $producto->id_categoria = $request->input('id_categoria');
+
+        $producto->save();
+
+        return redirect()->route('productos.index');
+    }
 }
